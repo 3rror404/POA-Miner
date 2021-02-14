0dc8f9f4cb91b348e664165ae378935a972cd836
lopeed_prcy <35898975+lopeed@users.noreply.github.com>
2021-02-14 19:50:49 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ DEPS="~/POA-Miner/win64_deps"
 autoreconf -fi -I${DEPS}/share/aclocal
 ./configure --host=x86_64-w64-mingw32 \
 CFLAGS="-DWIN32 -DCURL_STATICLIB -O3 -std=c99 -I${DEPS}/include -DPTW32_STATIC_LIB" \
---with-libcurl=${DEPS} LDFLAGS="-static -L${DEPS}/lib"
+LDFLAGS="-static -L${DEPS}/lib"
 make
