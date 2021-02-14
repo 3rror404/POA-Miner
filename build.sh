7e4ea9aa84f5addca5600774f1f08bea8e50bded
lopeed_prcy <35898975+lopeed@users.noreply.github.com>
2021-02-14 20:46:22 +0100

    update workflow

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-DEPS="~/win64_deps"
+DEPS="/home/runner/work/POA-Miner/POA-Miner/win64_deps"
 autoreconf -fi -I${DEPS}/share/aclocal
 ./configure --host=x86_64-w64-mingw32 \
 CFLAGS="-DWIN32 -DCURL_STATICLIB -O3 -std=c99 -I${DEPS}/include -DPTW32_STATIC_LIB" \
